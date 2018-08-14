d4c44e6053113f74e39dba6a68f61d7486da6913
Nelson Herbin <nelson.herbin@colibee.com>
2018-08-14 09:01:47 +0200

    add: routing files

---
+++ src/app/router.ts
@@ -0,0 +1,8 @@
+import {UIRouter} from "@uirouter/angular";
+import {Injectable} from "@angular/core";
+
+/**
+ * This function configures the router
+ */
+ 
+export function routeurConfig(uiRouter: UIRouter) {}
